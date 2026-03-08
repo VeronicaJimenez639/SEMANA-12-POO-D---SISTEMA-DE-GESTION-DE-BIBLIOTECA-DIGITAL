43fd64ad93ea8a5c2f1d65bc159133cdfaa14995
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-03-08 15:34:45 -0500

    Se agrega funcion para mostrar libros en consola

--- biblioteca_app/main.py
+++ biblioteca_app/main.py
@@ -1,3 +1,11 @@
 from modelos.libro import Libro
 from modelos.usuario import Usuario
 from servicios.biblioteca_servicio import BibliotecaServicio
+
+def mostrar_libros(libros):
+    # Esta función se crea para no repetir el mismo bloque varias veces.
+    if not libros:
+        print("No hay libros para mostrar.")
+    else:
+        for libro in libros:
+            print(libro)
