75cd5e85d67bbf7146c31f567e6983f52f0e84a2
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-03-08 15:37:51 -0500

    Se agrega estructura inicial del menu interactivo

--- biblioteca_app/main.py
+++ biblioteca_app/main.py
@@ -9,3 +9,25 @@ def mostrar_libros(libros):
     else:
         for libro in libros:
             print(libro)
+
+def menu():
+    # Aquí se crea el servicio principal del sistema.
+    # main.py solo controla la interacción con el usuario.
+    biblioteca = BibliotecaServicio()
+
+    while True:
+        print("\n--- BIBLIOTECA DIGITAL ---")
+        print("1. Agregar libro")
+        print("2. Eliminar libro")
+        print("3. Registrar usuario")
+        print("4. Eliminar usuario")
+        print("5. Prestar libro")
+        print("6. Devolver libro")
+        print("7. Buscar libros por título")
+        print("8. Buscar libros por autor")
+        print("9. Buscar libros por categoría")
+        print("10. Listar libros prestados de un usuario")
+        print("11. Listar libros disponibles")
+        print("0. Salir")
+
+        opcion = input("Seleccione una opción: ").strip()
