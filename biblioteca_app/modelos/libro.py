dde54c61d4775330beb5e0bcc42c431c5cf1e491
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-03-08 14:35:46 -0500

    Se agregan getters de la clase Libro

--- biblioteca_app/modelos/libro.py
+++ biblioteca_app/modelos/libro.py
@@ -6,4 +6,19 @@ class Libro:
         self._categoria = categoria       # La categoría se guarda como texto normal porque solo representa una característica del libro.
         self._isbn = isbn                 # El ISBN identifica de forma única a cada libro.
 
-        
+#Getters para acceder a los atributos del libro. 
+# No se definen setters porque no se espera que estos atributos cambien una vez creado el libro.
+
+    def get_titulo(self):
+        # Devuelve la primera posición de la tupla: el título.
+        return self._titulo_autor[0]
+
+    def get_autor(self):
+        # Devuelve la segunda posición de la tupla: el autor.
+        return self._titulo_autor[1]
+
+    def get_categoria(self):
+        return self._categoria
+
+    def get_isbn(self):
+        return self._isbn     
