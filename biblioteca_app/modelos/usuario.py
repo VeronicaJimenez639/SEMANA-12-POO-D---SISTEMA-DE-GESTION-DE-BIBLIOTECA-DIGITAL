0c0f8df366f950325a46f21b1e7eb211759f692d
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-03-08 15:00:07 -0500

    Se agregan getters de la clase Usuario

--- biblioteca_app/modelos/usuario.py
+++ biblioteca_app/modelos/usuario.py
@@ -4,3 +4,14 @@ class Usuario:
         self._id_usuario = id_usuario # ID único del usuario.
         self._libros_prestados = [] # Se usa lista porque los préstamos pueden cambiar: se agregan y se eliminan.
 
+# Getters para acceder a los atributos del usuario.
+    def get_nombre(self):
+        return self._nombre
+
+    def get_id_usuario(self):
+        return self._id_usuario
+
+    def get_libros_prestados(self):
+        # Se devuelve una copia de la lista para proteger el encapsulamiento.
+        # Así no se modifica directamente desde fuera de la clase.
+        return list(self._libros_prestados)
