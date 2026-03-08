f6321997ad7175dd081e06c9da3182b18be97084
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-03-08 15:16:44 -0500

    Se agrega metodo para registrar libros disponibles

--- biblioteca_app/servicios/biblioteca_servicio.py
+++ biblioteca_app/servicios/biblioteca_servicio.py
@@ -44,3 +44,13 @@ class BibliotecaServicio:
                     isbns_vistos.add(libro.get_isbn())
 
         return todos_los_libros
+    
+    def agregar_libro(self, libro):
+        isbn = libro.get_isbn()
+
+        # Se valida que el ISBN no exista ni en disponibles ni en préstamo.
+        if isbn in self._libros_disponibles or self._buscar_libro_prestado(isbn):
+            return "Ya existe un libro con ese ISBN."
+
+        self._libros_disponibles[isbn] = libro
+        return "Libro agregado correctamente."
